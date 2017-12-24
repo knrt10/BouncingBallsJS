4b1fe0e083964b7a35979fd9088af9cde162486c
knrt10 <tripathi.kautilya@gmail.com>
2017-12-24 21:28:45 +0000

    added user control

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var para = document.querySelector('p');
+var count = 0;
 var canvas = document.querySelector('canvas'); // need to select for our balls to output
 var context = canvas.getContext('2d'); //getting context from canvas
 var width = canvas.width = window.innerWidth ;
@@ -12,14 +14,23 @@ function random(min,max){
 
 // Need to create a constructor function as we need multiple balls
 
-function Ball(x , y , xV , yV , color , radius){
+function Shape(x, y, xV, yV, exists) {
   this.x = x;
   this.y = y;
   this.xV = xV;
   this.yV = yV;
+  this.exists = exists;
+}
+
+function Ball(x, y, xV, yV, exists, color, radius) {
+  Shape.call(this, x, y, xV, yV, exists);
+
   this.color = color;
   this.radius = radius;
 }
+
+Ball.prototype = Object.create(Shape.prototype);
+Ball.prototype.constructor = Ball;
 
 // Now we create a Ball
 
@@ -33,6 +44,8 @@ Ball.prototype.create = function(){
 
 // Now we need to update the conditons in ball
 /*
+
+
 
 This function is going to check whenever the balls hit the right,left,bottom,top
 it will reverse its direction
@@ -60,6 +73,40 @@ Ball.prototype.update = function() {
   this.y += this.yV;
 }
 
+// define EvilCircle collision detection
+
+function EvilCircle(x, y, exists) {
+  Shape.call(this, x, y, exists);
+
+  this.color = 'white';
+  this.radius = 10;
+  this.xV = 20;
+  this.yV = 20;
+}
+
+EvilCircle.prototype = Object.create(Shape.prototype);
+EvilCircle.prototype.constructor = EvilCircle;
+
+
+EvilCircle.prototype.collisionDetect = function() {
+  for(var j = 0; j < balls.length; j++) {
+    if( balls[j].exists ) {
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.radius + balls[j].radius) {
+        balls[j].exists = false;
+        count--;
+        if(count === 0){
+          alert('Game Over');
+        }
+        para.textContent = 'Ball count: ' + count;
+      }
+    }
+  }
+};
+
 
 // Now time to create our balls
 
@@ -81,6 +128,58 @@ Ball.prototype.colorChange = function(){
 }
 
 
+
+// define EvilCircle draw method
+
+EvilCircle.prototype.draw = function() {
+  context.beginPath();
+  context.strokeStyle = this.color;
+  context.lineWidth = 4;
+  context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+  context.stroke();
+};
+
+// define EvilCircle checkBounds method
+
+EvilCircle.prototype.checkBounds = function() {
+  if((this.x + this.radius) >= width) {
+    this.x -= this.radius;
+  }
+
+  if((this.x - this.radius) <= 0) {
+    this.x += this.radius;
+  }
+
+  if((this.y + this.radius) >= height) {
+    this.y -= this.radius;
+  }
+
+  if((this.y - this.radius) <= 0) {
+    this.y += this.radius;
+  }
+};
+
+// define EvilCircle setControls method
+
+EvilCircle.prototype.setControls = function() {
+  var _this = this;
+  window.onkeydown = function(e) {
+    if(e.keyCode === 65) { // a
+      _this.x -= _this.xV;
+    } else if(e.keyCode === 68) { // d
+      _this.x += _this.xV;
+    } else if(e.keyCode === 87) { // w
+      _this.y -= _this.yV;
+    } else if(e.keyCode === 83) { // s
+      _this.y += _this.yV;
+    }
+  };
+};
+
+var evil = new EvilCircle(random(0,width), random(0,height), true);
+evil.setControls();
+
+
 function repeat(){
   context.fillStyle = 'rgba(0,0,0,0.25)';
   context.fillRect(0, 0 ,width , height);
@@ -91,16 +190,24 @@ function repeat(){
       random(0,height),
       random(-7,7),
       random(-7,7),
+      true,
       'rgb(' + random(0,255) +','+ random(0,255)+','+random(0,255) + ')',
       random(10,20)
     );
     balls.push(ball);
+    count++;
+     para.textContent = 'Ball count: ' + count;
   }
   for(var i=0 ; i<balls.length; i++){
-    balls[i].create();
-    balls[i].update();
-    balls[i].colorChange();
+    if(balls[i].exists){
+      balls[i].create();
+      balls[i].update();
+      balls[i].colorChange();
+    }
   }
+  evil.draw();
+  evil.checkBounds();
+  evil.collisionDetect();
   requestAnimationFrame(repeat);
 }
 
